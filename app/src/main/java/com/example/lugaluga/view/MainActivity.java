3d7902feb2a8cb91135133394eb85281c29e8187
Maria Olivia <maria.vaz8@aluno.senai.br>
2024-05-09 15:14:41 -0300

    aula dia 09/05

--- app/src/main/java/com/example/lugaluga/view/MainActivity.java
+++ app/src/main/java/com/example/lugaluga/view/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.lugaluga.view;
 
+import static com.example.lugaluga.R.id.my_toolbar;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -35,11 +38,16 @@ public class MainActivity extends AppCompatActivity {
     private List<Produto> produtoList = new ArrayList<>();
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(my_toolbar);
+        setSupportActionBar(myToolbar);
+
 
         recyclerView = findViewById(R.id.listaProdutos);
 
--- app/src/main/java/com/example/lugaluga/view/ProdutoActivity.java
+++ app/src/main/java/com/example/lugaluga/view/ProdutoActivity.java
@@ -1,10 +1,16 @@
 package com.example.lugaluga.view;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,16 +23,24 @@ public class ProdutoActivity extends AppCompatActivity {
     private Produto produto;
 
     private TextView nomeProduto, qtdProduto, precoProduto, descProduto;
+
+    private Button alugaBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_produto);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
         nomeProduto = findViewById(R.id.ProdutoTv);
         qtdProduto = findViewById(R.id.qtdProdutoTv);
         precoProduto = findViewById(R.id.precoProdutoTv);
         descProduto = findViewById(R.id.descricaoTv);
+
+        alugaBtn = findViewById(R.id.alugaBtn);
 
         produto = getIntent().getExtras().getParcelable("produto");
 
@@ -34,6 +48,31 @@ public class ProdutoActivity extends AppCompatActivity {
         qtdProduto.setText(produto.getQuantidade());
         precoProduto.setText(String.valueOf(produto.getPreco()));
         descProduto.setText(produto.getDescricao());
+
+        alugaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ProdutoActivity.this);
+                builder.setMessage("Deseja realmente alugar este produto?");
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(ProdutoActivity.this, "Concluido", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
 
 
     }
